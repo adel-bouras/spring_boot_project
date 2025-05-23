f65cf510e61aff024cd96eba7e8699d7c885ea97
adel-bouras <eyyad.kareem1212@gmail.com>
2025-05-23 11:56:23 +0100

    init

--- src/main/java/com/example/creche/service/parentService.java
+++ src/main/java/com/example/creche/service/parentService.java
@@ -2,8 +2,24 @@ package com.example.creche.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.creche.repository.enfantRepository;
+import com.example.creche.repository.parentRepository;
+import com.example.creche.repository.responseRepository;
+
 @Service
 public class parentService {
 
+    private final parentRepository parentrepository;
+    private final responseRepository responserepository;
+    private final enfantRepository enfantrepository;
+
+    public parentService(parentRepository parentrepository , responseRepository responserepository , enfantRepository enfantrepository){
+        this.enfantrepository = enfantrepository;
+        this.parentrepository = parentrepository;
+        this.responserepository = responserepository;
+    }
+
+
+    
     
 }
